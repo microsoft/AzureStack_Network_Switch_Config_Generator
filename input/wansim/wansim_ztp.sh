05a66872407b108a003dee1efe281bf0c28fbdf0
Nick Liu <105009141+liunick-msft@users.noreply.github.com>
2023-09-05 19:41:53 +0000

    add WANSIM block in input.json

---
+++ input/wansim/wansim_ztp.sh
@@ -0,0 +1,20 @@
+# Init
+sudo apt-get update
+sudo apt-get install -y net-tools frr iperf3 traceroute
+HOSTNAME="WAN-SIM"
+sudo hostnamectl set-hostname $HOSTNAME
+# sudo nano /etc/hosts
+
+# Config Interface
+sudo cp ./30_netplan_wansim.yaml /etc/netplan/30_netplan_wansim.yaml
+sudo netplan apply
+
+# Config FRR
+sudo cp ./wansim_daemons /etc/frr/daemons
+sudo service frr restart
+sudo cp ./wansim_frr.conf /etc/frr/frr.conf
+sudo service frr restart
+
+# Post-Validation
+sudo chmod +x postValidation.sh
+sudo ./postValidation.sh
