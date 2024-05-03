decd3a58953e37c6c48bde4abbf6b0c488257944
Nick Liu (from Dev Box) <liunick@microsoft.com>
2024-05-03 10:46:43 -0700

    update golden config

--- src/test/goldenConfig/s46r21-definition/wansim_vm_config/monitor_tc_rules.py
+++ src/test/goldenConfig/s46r21-definition/wansim_vm_config/monitor_tc_rules.py
@@ -32,3 +32,30 @@ for intf in intfs:
             elif int(class_id.split(':')[1]) >= 10:
                 env_id = int(class_id.split(':')[1]) // 10
             print(f"tc_bw_rate_bits{{intf=\"{intf}\", env_id=\"{env_id}\", rate=\"{rate}\"}} {rate}")
+
+
+# sudo tc qdisc show dev gre1
+# qdisc htb 1a1a: root refcnt 2 r2q 10 default 0x1 direct_packets_stat 0 direct_qlen 1000
+# qdisc netem 50: parent 1a1a:50 limit 1000 delay 60ms loss 1%
+print("# HELP tc_delay_ms TC Rule for delay by ENV")
+print("# TYPE tc_delay_ms gauge")
+
+print("# HELP tc_loss_percentage TC Rule for loss by ENV")
+print("# TYPE tc_loss_percentage gauge")
+
+for intf in intfs:
+    output = subprocess.check_output(f"sudo tc qdisc show dev {intf}", shell=True).decode('utf-8')
+    lines = output.split('\n')
+
+    for line in lines:
+        if "qdisc netem" in line:
+            pattern = r'netem (\d+):(?:.*delay (\d+)ms)?(?:.*loss (\d+)%)?'
+            matches = re.findall(pattern, line)
+            class_id = matches[0][0] if matches else None
+            delay = next((matches[0][1] for match in matches if matches[0][1]), None)
+            loss = next((matches[0][2] for match in matches if matches[0][2]), None)
+            env_id = int(class_id) // 10
+            if delay:
+                print(f"tc_delay_ms{{intf=\"{intf}\", env_id=\"{env_id}\"}} {delay}")
+            if loss:
+                print(f"tc_loss_percentage{{intf=\"{intf}\", env_id=\"{env_id}\"}} {loss}")
